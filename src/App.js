545e4f9c5e9a509da04031b6f1a4606a7760fbe8
Linda Kuang <linda.jk@gmx.de>
2022-06-27 16:34:47 +0200

    Fetch & display API

--- src/App.js
+++ src/App.js
@@ -1,3 +1,35 @@
+import {useEffect, useState} from 'react';
+
 export default function App() {
-  return <div>App goes here</div>;
+  const [events, setEvents] = useState([]);
+  useEffect(() => {
+    async function loadEvents() {
+      try {
+        const response = await fetch(
+          'https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=5I30P7cDjRjyvGDo67WGAE2BhhTwRxXt'
+        );
+        const data = await response.json();
+        setEvents(data._embedded.events);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    loadEvents();
+  }, []);
+  // console.log(events);
+  return (
+    <main className="App">
+      <ul>
+        {events.map(event => (
+          <li key={event.id}>
+            {event.name}
+            {event.name}
+            {event.dates.start.localDate}
+            {event.dates.start.localTime}
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
