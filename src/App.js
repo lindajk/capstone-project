1cdcc03493ad0cd42f7e19a728d2adccb51cea37
Linda Kuang <linda.jk@gmx.de>
2022-06-29 16:16:38 +0200

    Add dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import CityButton from './components/CityButton';
 import EventList from './components/EventList';
 
 export default function App() {
   return (
     <main className="App">
+      <CityButton></CityButton>
       <EventList></EventList>;
     </main>
   );
---
+++ src/components/CityButton.stories.js
@@ -0,0 +1,8 @@
+import CityButton from './CityButton.js';
+
+export default {
+  title: 'Components/CityButton',
+  component: CityButton,
+};
+
+export const Default = () => <CityButton />;
