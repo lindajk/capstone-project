20b5b2ebcd02fafeff9008030ebe3eaf06c6c79d
Linda Kuang <linda.jk@gmx.de>
2022-07-04 17:00:22 +0200

    Update filter styles

--- src/components/CityButton.js
+++ src/components/CityButton.js
@@ -30,31 +30,31 @@ export default function CityButton({selectedOption, selectEvents}) {
   );
 }
 
-const DropDownContainer = styled('div')`
+const DropDownContainer = styled.div`
   width: 50%;
   //margin: 0 auto;
 `;
 
-const DropDownHeader = styled('div')`
+const DropDownHeader = styled.div`
   margin-bottom: 0.8em;
   padding: 0.4em 2em 0.4em 1em;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
   font-weight: 500;
   font-size: 1.3rem;
-  color: #3faffa;
-  background: #ffffff;
+  color: blue;
+  background: white;
 `;
 
-const DropDownListContainer = styled('div')``;
+const DropDownListContainer = styled.div``;
 
-const DropDownList = styled('ul')`
+const DropDownList = styled.ul`
   padding: 0;
   margin: 0;
   padding-left: 1em;
-  background: #ffffff;
-  border: 2px solid #e5e5e5;
+  background: white;
+  border: 2px solid lightgrey;
   box-sizing: border-box;
-  color: #3faffa;
+  color: blue;
   font-size: 1.3rem;
   font-weight: 500;
   &:first-child {
@@ -62,7 +62,7 @@ const DropDownList = styled('ul')`
   }
 `;
 
-const ListItem = styled('li')`
+const ListItem = styled.li`
   list-style: none;
   margin-bottom: 0.8em;
 `;
